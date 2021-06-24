39f29d4f65edeac6020f6ea66a0ad774539464f4
Emma Strickland <emlstrick@gmail.com>
2021-06-24 18:51:35 -0400

    remove react logo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,10 +17,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
         <BeerList />
       </header>
-      <p>This is a paragraph</p>
     </div>
   );
 }
